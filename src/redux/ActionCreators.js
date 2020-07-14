1d0c5929dc20122d39bd72276f25e5f80a40d7e9
technophile22 <raghavmangal22@gmail.com>
2020-07-14 17:16:01 +0530

    Fetch Handling Errors

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -17,9 +17,24 @@ export const fetchDishes = () => (dispatch) => {
     dispatch(dishesLoading(true));
 
     return fetch(baseUrl + 'dishes')
+        .then(response => {
+            if(response.ok) {
+                return response;
+            }
+            else{
+                var error = new Error('Error ' + response.status + ': '+ response.statusText);
+                error.response = response;
+                throw error;
+            }
+        },
+        //when you hear nothing from server
+        error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+        })
         .then(response => response.json())
-        .then(dishes => dispatch(addDishes(dishes)));
-
+        .then(dishes => dispatch(addDishes(dishes)))
+        .catch(error => dispatch(dishesFailed(error.message)));
 }
 
 
@@ -45,8 +60,24 @@ export const fetchComments = () => (dispatch) => {
 
 
     return fetch(baseUrl + 'comments')
+    .then(response => {
+        if(response.ok) {
+            return response;
+        }
+        else{
+            var error = new Error('Error ' + response.status + ': '+ response.statusText);
+            error.response = response;
+            throw error;
+        }
+    },
+    //when you hear nothing from server
+    error => {
+        var errmess = new Error(error.message);
+        throw errmess;
+    })
         .then(response => response.json())
-        .then(comments => dispatch(addComments(comments)));
+        .then(comments => dispatch(addComments(comments)))
+        .catch(error => dispatch(commentsFailed(error.message)));
 
 }
 
@@ -66,9 +97,24 @@ export const fetchPromos = () => (dispatch) => {
     dispatch(promosLoading());
 
     return fetch(baseUrl + 'promotions')
+    .then(response => {
+        if(response.ok) {
+            return response;
+        }
+        else{
+            var error = new Error('Error ' + response.status + ': '+ response.statusText);
+            error.response = response;
+            throw error;
+        }
+    },
+    //when you hear nothing from server
+    error => {
+        var errmess = new Error(error.message);
+        throw errmess;
+    })
         .then(response => response.json())
-        .then(promos => dispatch(addPromos(promos)));
-
+        .then(promos => dispatch(addPromos(promos)))
+        .catch(error => dispatch(promosFailed(error.message)));
 }
 
 
