98c9d8750f370b083fcebc1ceb33ddc7c1ae97e1
technophile22 <raghavmangal22@gmail.com>
2020-07-15 11:02:17 +0530

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import {Link} from 'react-router-dom';
 import {Control, LocalForm, Errors} from 'react-redux-form';
 import {Loading } from './LoadingComponent';
 import {baseUrl} from '../shared/baseUrl';
+import {FadeTransform, Fade, Stagger} from 'react-animation-components';
 
 const required =(val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -132,13 +133,18 @@ class CommentForm extends Component{
         if (dish != null) {
             return (
                 <div className='col-12 col-md-5 m-1'>
-                    <Card>
-                        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
+                    <FadeTransform in
+                        transformProps ={{
+                        exitTransform:'scale(0.5) translateY(-50%)'
+                    }}>
+                        <Card>
+                            <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
+                            <CardBody>
+                                <CardTitle>{dish.name}</CardTitle>
+                                <CardText>{dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </FadeTransform>
                 </div>
             )
         }
@@ -160,6 +166,7 @@ class CommentForm extends Component{
 
                 const cmnts = comments.map((comment) => {
                     return (
+                        <Fade in>
                         <li key={comment.id}>
                             <p>{comment.comment}</p>
                             <p>-- {comment.author},
@@ -171,13 +178,16 @@ class CommentForm extends Component{
                                 }).format(new Date(comment.date))}
                             </p>
                         </li>
+                        </Fade>
                     );
                 });
                 return (
                     <div className='col-12 col-md-5 m-1'>
                         <h4> Comments </h4>
                         <ul className='list-unstyled'>
+                            <Stagger in>
                             {cmnts}
+                            </Stagger>
                         </ul>
                         <CommentForm dishId={dishId} postComment={postComment} />
                        
