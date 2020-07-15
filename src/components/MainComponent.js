cdb9b7f3a8ed90c3620170ba32e9e8dfbb8bcdb2
technophile22 <raghavmangal22@gmail.com>
2020-07-15 10:28:50 +0530

    React Animations

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,7 +10,7 @@ import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import { connect } from 'react-redux';
 import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
-
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
 const mapStateToProps  = state => {
     return {
       dishes : state.dishes,
@@ -79,7 +79,9 @@ class Main extends Component {
     return (
       <div >
         <Header />
-        <Switch>
+        <TransitionGroup>
+          <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+        <Switch location={this.props.location}>
             <Route path="/home" component= {HomePage} />
             <Route exact path="/aboutus" component={ () => <About leaders={this.props.leaders} />} />
             <Route exact path="/menu" component={ () => <Menu dishes={this.props.dishes} />} />
@@ -87,6 +89,8 @@ class Main extends Component {
             <Route exact path="/contactus" component={ () => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} /> 
             <Redirect to="/home" />
         </Switch>
+        </CSSTransition>
+        </TransitionGroup>
         <Footer />
       </div>
     );
